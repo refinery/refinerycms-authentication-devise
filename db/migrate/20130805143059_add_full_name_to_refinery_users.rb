aa4671c10a4620157bd6fccfd22bba78ae0772a4
Roman Krylov <xtsidx@gmail.com>
2017-08-30 09:14:10 +0300

    Specify the Rails release the migration was written for

--- db/migrate/20130805143059_add_full_name_to_refinery_users.rb
+++ db/migrate/20130805143059_add_full_name_to_refinery_users.rb
@@ -1,4 +1,4 @@
-class AddFullNameToRefineryUsers < ActiveRecord::Migration
+class AddFullNameToRefineryUsers < ActiveRecord::Migration[4.2]
   def change
     add_column :refinery_users, :full_name, :string
   end
